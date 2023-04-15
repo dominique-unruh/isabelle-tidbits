b9b5dd6a6b745019981347bbf0dd855631038348
Dominique Unruh <unruh@ut.ee>
2023-04-15 16:53:41 +0300

    More code cleanup.

--- Wlog/Wlog_Examples.thy
+++ Wlog/Wlog_Examples.thy
@@ -41,11 +41,23 @@ proof -
 
   wlog geq: "a > b" generalizing a b keeping neq3
   proof (cases "a > b")
-  case True show ?thesis using True hypothesis by blast
-  next case False show ?thesis proof (rule aux, cases rule:hypothesis[of a b])
-    case geq show ?case using False neq2 by blast
-    next case neq3 show ?case using neq2 by auto
-    next assume "1 \<le> b + a" then show "1 \<le> a + b" by linarith
+    case True 
+    show ?thesis
+      using True hypothesis by blast
+  next
+    case False
+    show ?thesis
+    proof (rule aux, cases rule:hypothesis[of a b])
+      case geq
+      show ?case
+        using False lost.neq2
+        by simp
+    next
+      case neq3 
+      show ?case using lost.neq2 by auto
+    next 
+      assume "1 \<le> b + a" 
+      then show "1 \<le> a + b" by linarith
     qed
   qed
 
@@ -56,6 +68,7 @@ proof -
  (* apply (tactic \<open>resolve_tac @{context} [neq2'] 1\<close>) using assms by auto *)
 
   wlog tmp: "a=a" generalizing a b keeping geq 
+    print_theorems
     using hypothesis neq geq by metis
 
   from geq have "a \<ge> 1" by auto
